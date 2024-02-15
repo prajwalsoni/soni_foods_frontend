095903cebd1eeb5c80f279d140e26405e2b2b489
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 22:49:10 +0530

    Added OrderCard

--- src/customers/components/Order/OrderCard.jsx
+++ src/customers/components/Order/OrderCard.jsx
@@ -1,25 +1,25 @@
-// import { Button, Card } from "@mui/material";
-// import React from "react";
+import { Button, Card } from "@mui/material";
+import React from "react";
 
-// const OrderCard = ({order}) => {
-//   return (
-//     <Card className="flex justify-between items-center p-5 ">
-//       <div className="flex items-center space-x-5">
-//         <img
-//           className="h-16 w-16"
-//           src={order.menuItem.imageUrl}
-//           alt=""
-//         />
-//         <div>
-//           <p>{order.menuItem.name}</p>
-//           <p className="text-gray-400">₹{order.menuItem.price}</p>
-//         </div>
-//       </div>
-//       <div>
-//         <Button variant="contained">Track</Button>
-//       </div>
-//     </Card>
-//   );
-// };
+const OrderCard = ({order}) => {
+  return (
+    <Card className="flex justify-between items-center p-5 ">
+      <div className="flex items-center space-x-5">
+        <img
+          className="h-16 w-16"
+          src={order.menuItem.imageUrl}
+          alt=""
+        />
+        <div>
+          <p>{order.menuItem.name}</p>
+          <p className="text-gray-400">₹{order.menuItem.price}</p>
+        </div>
+      </div>
+      <div>
+        <Button variant="contained">Track</Button>
+      </div>
+    </Card>
+  );
+};
 
-// export default OrderCard;
+export default OrderCard;
