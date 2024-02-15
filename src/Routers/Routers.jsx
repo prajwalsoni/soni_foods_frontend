8ecf2b901c19032589818f4ee735e6a785efad39
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 22:57:54 +0530

    Added CustomerRoutes.jsx and Routers

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -1,22 +1,22 @@
-// import React from "react";
-// import { Route, Routes } from "react-router-dom";
-// import CustomerRoutes from "./CustomerRoutes";
-// import Admin from "../Admin/Admin";
-// import AdminDashboard from "../Admin/Dashboard/AdminDashboard";
-// import SuperAdmin from "../SuperAdmin/SuperAdmin";
-// import { useSelector } from "react-redux";
-// import NotFound from "../customers/pages/NotFound/NotFound";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import CustomerRoutes from "./CustomerRoutes";
+import Admin from "../Admin/Admin";
+import AdminDashboard from "../Admin/Dashboard/AdminDashboard";
+import SuperAdmin from "../SuperAdmin/SuperAdmin";
+import { useSelector } from "react-redux";
+import NotFound from "../customers/pages/NotFound/NotFound";
 
-// const Routers = () => {
-//   const {auth}=useSelector(store=>store)
-//   return (
-//     <Routes>
-//       <Route path="/*" element={<CustomerRoutes />} />
-//       <Route exact path="/admin" element={auth.user?.role==="ROLE_ADMIN"?<AdminDashboard />:<NotFound/>} />
-//       <Route exact path="/admin/restaurants/:id/*" element={<Admin />} />
-//       <Route exact path="/super-admin/*" element={auth.user?.role==="SUPER_ADMIN"?<SuperAdmin />:<NotFound/>} />
-//     </Routes>
-//   );
-// };
+const Routers = () => {
+  const {auth}=useSelector(store=>store)
+  return (
+    <Routes>
+      <Route path="/*" element={<CustomerRoutes />} />
+      <Route exact path="/admin" element={auth.user?.role==="ROLE_ADMIN"?<AdminDashboard />:<NotFound/>} />
+      <Route exact path="/admin/restaurants/:id/*" element={<Admin />} />
+      <Route exact path="/super-admin/*" element={auth.user?.role==="SUPER_ADMIN"?<SuperAdmin />:<NotFound/>} />
+    </Routes>
+  );
+};
 
-// export default Routers;
+export default Routers;
