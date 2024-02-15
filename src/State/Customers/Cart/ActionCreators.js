2e634832336b63fea3e144b36393e897451a3230
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 22:59:35 +0530

    Added cart redux files

--- src/State/Customers/Cart/ActionCreators.js
+++ src/State/Customers/Cart/ActionCreators.js
@@ -1,34 +1,34 @@
-// import * as actionTypes from './ActionTypes';
+import * as actionTypes from './ActionTypes';
 
-// // Find Cart Actions
-// export const findCartRequest = () => ({
-//   type: actionTypes.FIND_CART_REQUEST,
-// });
+// Find Cart Actions
+export const findCartRequest = () => ({
+  type: actionTypes.FIND_CART_REQUEST,
+});
 
-// export const findCartSuccess = (cart) => ({
-//   type: actionTypes.FIND_CART_SUCCESS,
-//   payload: cart,
-// });
+export const findCartSuccess = (cart) => ({
+  type: actionTypes.FIND_CART_SUCCESS,
+  payload: cart,
+});
 
-// export const findCartFailure = (error) => ({
-//   type: actionTypes.FIND_CART_FAILURE,
-//   payload: error,
-// });
+export const findCartFailure = (error) => ({
+  type: actionTypes.FIND_CART_FAILURE,
+  payload: error,
+});
 
 
-// // Get All Cart Items Actions
-// export const getAllCartItemsRequest = () => ({
-//     type: actionTypes.GET_ALL_CART_ITEMS_REQUEST,
-//   });
+// Get All Cart Items Actions
+export const getAllCartItemsRequest = () => ({
+    type: actionTypes.GET_ALL_CART_ITEMS_REQUEST,
+  });
   
-//   export const getAllCartItemsSuccess = (cartItems) => ({
-//     type: actionTypes.GET_ALL_CART_ITEMS_SUCCESS,
-//     payload: cartItems,
-//   });
+  export const getAllCartItemsSuccess = (cartItems) => ({
+    type: actionTypes.GET_ALL_CART_ITEMS_SUCCESS,
+    payload: cartItems,
+  });
   
-//   export const getAllCartItemsFailure = (error) => ({
-//     type: actionTypes.GET_ALL_CART_ITEMS_FAILURE,
-//     payload: error,
-//   });
+  export const getAllCartItemsFailure = (error) => ({
+    type: actionTypes.GET_ALL_CART_ITEMS_FAILURE,
+    payload: error,
+  });
 
   
--- src/State/Customers/Cart/cart.action.js
+++ src/State/Customers/Cart/cart.action.js
@@ -1,95 +1,95 @@
 
-// import { api } from "../../../config/api";
-// import {
-//   findCartFailure,
-//   findCartRequest,
-//   findCartSuccess,
-//   getAllCartItemsFailure,
-//   getAllCartItemsRequest,
-//   getAllCartItemsSuccess,
-// } from "./ActionCreators";
-// import { ADD_ITEM_TO_CART_FAILURE, ADD_ITEM_TO_CART_REQUEST, ADD_ITEM_TO_CART_SUCCESS, REMOVE_CARTITEM_FAILURE, REMOVE_CARTITEM_REQUEST, REMOVE_CARTITEM_SUCCESS, UPDATE_CARTITEM_FAILURE, UPDATE_CARTITEM_REQUEST, UPDATE_CARTITEM_SUCCESS } from "./ActionTypes";
+import { api } from "../../../config/api";
+import {
+  findCartFailure,
+  findCartRequest,
+  findCartSuccess,
+  getAllCartItemsFailure,
+  getAllCartItemsRequest,
+  getAllCartItemsSuccess,
+} from "./ActionCreators";
+import { ADD_ITEM_TO_CART_FAILURE, ADD_ITEM_TO_CART_REQUEST, ADD_ITEM_TO_CART_SUCCESS, REMOVE_CARTITEM_FAILURE, REMOVE_CARTITEM_REQUEST, REMOVE_CARTITEM_SUCCESS, UPDATE_CARTITEM_FAILURE, UPDATE_CARTITEM_REQUEST, UPDATE_CARTITEM_SUCCESS } from "./ActionTypes";
 
-// export const findCart = (token) => {
-//   return async (dispatch) => {
-//     dispatch(findCartRequest());
-//     try {
-//       const response = await api.get(`/api/cart/`,{
-//         headers: {
-//             Authorization: `Bearer ${token}`,
-//           },
-//       });
-//       dispatch(findCartSuccess(response.data));
-//     } catch (error) {
-//       dispatch(findCartFailure(error));
-//     }
-//   };
-// };
+export const findCart = (token) => {
+  return async (dispatch) => {
+    dispatch(findCartRequest());
+    try {
+      const response = await api.get(`/api/cart/`,{
+        headers: {
+            Authorization: `Bearer ${token}`,
+          },
+      });
+      dispatch(findCartSuccess(response.data));
+    } catch (error) {
+      dispatch(findCartFailure(error));
+    }
+  };
+};
 
-// export const getAllCartItems = (reqData) => {
-//   return async (dispatch) => {
-//     dispatch(getAllCartItemsRequest());
-//     try {
-//       const response = await api.get(`/api/carts/${reqData.cartId}/items`,{
-//         headers: {
-//             Authorization: `Bearer ${reqData.token}`,
-//           },
-//       });
-//       dispatch(getAllCartItemsSuccess(response.data));
-//     } catch (error) {
-//       dispatch(getAllCartItemsFailure(error));
-//     }
-//   };
-// };
+export const getAllCartItems = (reqData) => {
+  return async (dispatch) => {
+    dispatch(getAllCartItemsRequest());
+    try {
+      const response = await api.get(`/api/carts/${reqData.cartId}/items`,{
+        headers: {
+            Authorization: `Bearer ${reqData.token}`,
+          },
+      });
+      dispatch(getAllCartItemsSuccess(response.data));
+    } catch (error) {
+      dispatch(getAllCartItemsFailure(error));
+    }
+  };
+};
 
-// export const addItemToCart= (reqData) => {
-//   return async (dispatch) => {
-//     dispatch({type:ADD_ITEM_TO_CART_REQUEST});
-//     try {
-//       const {data} = await api.put(`/api/cart/add`,reqData.cartItem,{
-//         headers: {
-//             Authorization: `Bearer ${reqData.token}`,
-//           },
-//       });
-//       console.log("add item to cart ",data)
-//       dispatch({type:ADD_ITEM_TO_CART_SUCCESS, payload:data});
+export const addItemToCart= (reqData) => {
+  return async (dispatch) => {
+    dispatch({type:ADD_ITEM_TO_CART_REQUEST});
+    try {
+      const {data} = await api.put(`/api/cart/add`,reqData.cartItem,{
+        headers: {
+            Authorization: `Bearer ${reqData.token}`,
+          },
+      });
+      console.log("add item to cart ",data)
+      dispatch({type:ADD_ITEM_TO_CART_SUCCESS, payload:data});
       
-//     } catch (error) {
-//       console.log("catch error ",error)
-//       dispatch({type:ADD_ITEM_TO_CART_FAILURE,payload:error.message});
-//     }
-//   };
-// };
+    } catch (error) {
+      console.log("catch error ",error)
+      dispatch({type:ADD_ITEM_TO_CART_FAILURE,payload:error.message});
+    }
+  };
+};
 
-// export const updateCartItem= (reqData) => {
-//   return async (dispatch) => {
-//     dispatch({type:UPDATE_CARTITEM_REQUEST});
-//     try {
-//       const {data} = await api.put(`/api/cart-item/update`,reqData);
-//       console.log("update cartItem ",data)
-//       dispatch({type:UPDATE_CARTITEM_SUCCESS, payload:data});
+export const updateCartItem= (reqData) => {
+  return async (dispatch) => {
+    dispatch({type:UPDATE_CARTITEM_REQUEST});
+    try {
+      const {data} = await api.put(`/api/cart-item/update`,reqData);
+      console.log("update cartItem ",data)
+      dispatch({type:UPDATE_CARTITEM_SUCCESS, payload:data});
       
-//     } catch (error) {
-//       console.log("catch error ",error)
-//       dispatch({type:UPDATE_CARTITEM_FAILURE,payload:error.message});
-//     }
-//   };
-// };
+    } catch (error) {
+      console.log("catch error ",error)
+      dispatch({type:UPDATE_CARTITEM_FAILURE,payload:error.message});
+    }
+  };
+};
 
-// export const removeCartItem= (cartItemId) => {
-//   return async (dispatch) => {
-//     dispatch({type:REMOVE_CARTITEM_REQUEST});
-//     try {
-//       const {data} = await api.delete(`/api/cart-item/${cartItemId}/remove`);
-//       console.log("remove cartItem ",data)
-//       dispatch({type:REMOVE_CARTITEM_SUCCESS, payload:cartItemId});
+export const removeCartItem= (cartItemId) => {
+  return async (dispatch) => {
+    dispatch({type:REMOVE_CARTITEM_REQUEST});
+    try {
+      const {data} = await api.delete(`/api/cart-item/${cartItemId}/remove`);
+      console.log("remove cartItem ",data)
+      dispatch({type:REMOVE_CARTITEM_SUCCESS, payload:cartItemId});
       
-//     } catch (error) {
-//       console.log("catch error ",error)
-//       dispatch({type:REMOVE_CARTITEM_FAILURE,payload:error.message});
-//     }
-//   };
-// };
+    } catch (error) {
+      console.log("catch error ",error)
+      dispatch({type:REMOVE_CARTITEM_FAILURE,payload:error.message});
+    }
+  };
+};
 
 
 
