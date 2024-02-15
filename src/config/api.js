49df4f650a5c7b6825925f4ac9bb33d0989d10b0
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 22:43:24 +0530

    Added api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,14 +1,14 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// export const API_URL = "http://localhost:5454";
+export const API_URL = "http://localhost:5454";
 
-// export const api = axios.create({
-//   baseURL: API_URL, 
-//   timeout: 5000, 
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Authorization':`Bearer ${localStorage.getItem("jwt")}` 
-//   },
-// });
+export const api = axios.create({
+  baseURL: API_URL, 
+  timeout: 5000, 
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization':`Bearer ${localStorage.getItem("jwt")}` 
+  },
+});
 
 
