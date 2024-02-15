23d9d0d4911af5b818e06f230cb4cb8b6ca7dc7c
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 23:01:53 +0530

    Added Redux superAdmin

--- src/State/Customers/Orders/Action.js
+++ src/State/Customers/Orders/Action.js
@@ -1,39 +1,39 @@
-// import { api } from "../../../config/api";
-// import { createOrderFailure, createOrderRequest, createOrderSuccess, getUsersOrdersFailure, getUsersOrdersRequest, getUsersOrdersSuccess } from "./ActionCreators";
+import { api } from "../../../config/api";
+import { createOrderFailure, createOrderRequest, createOrderSuccess, getUsersOrdersFailure, getUsersOrdersRequest, getUsersOrdersSuccess } from "./ActionCreators";
 
 
-// export const createOrder = (reqData) => {
-//   return async (dispatch) => {
-//     dispatch(createOrderRequest());
-//     try {
-//       const {data} = await api.post('/api/order', reqData.order,{
-//         headers: {
-//             Authorization: `Bearer ${reqData.token}`,
-//           },
-//       });
-//       if(data.payment_url){
-//         window.location.href=data.payment_url;
-//       }
-//       console.log("created order data",data)
-//       dispatch(createOrderSuccess(data));
-//     } catch (error) {
-//       console.log("error ",error)
-//       dispatch(createOrderFailure(error));
-//     }
-//   };
-// };
+export const createOrder = (reqData) => {
+  return async (dispatch) => {
+    dispatch(createOrderRequest());
+    try {
+      const {data} = await api.post('/api/order', reqData.order,{
+        headers: {
+            Authorization: `Bearer ${reqData.token}`,
+          },
+      });
+      if(data.payment_url){
+        window.location.href=data.payment_url;
+      }
+      console.log("created order data",data)
+      dispatch(createOrderSuccess(data));
+    } catch (error) {
+      console.log("error ",error)
+      dispatch(createOrderFailure(error));
+    }
+  };
+};
 
 
-// export const getUsersOrders = () => {
-//   return async (dispatch) => {
-//     dispatch(getUsersOrdersRequest());
-//     try {
-//       const {data} = await api.get(`/api/order/user`);
-//       console.log("users order ",data)
-//       dispatch(getUsersOrdersSuccess(data));
-//     } catch (error) {
-//       dispatch(getUsersOrdersFailure(error));
-//     }
-//   };
-// };
+export const getUsersOrders = () => {
+  return async (dispatch) => {
+    dispatch(getUsersOrdersRequest());
+    try {
+      const {data} = await api.get(`/api/order/user`);
+      console.log("users order ",data)
+      dispatch(getUsersOrdersSuccess(data));
+    } catch (error) {
+      dispatch(getUsersOrdersFailure(error));
+    }
+  };
+};
 
