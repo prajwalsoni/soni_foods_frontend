39f25d4896f7d90024c111e4d009c51d127701c2
prajwalsoni <prajwalsonics@gmail.com>
2024-02-15 22:42:31 +0530

    Added RestaurantsOrder

--- src/Admin/Orders/RestaurantsOrder.jsx
+++ src/Admin/Orders/RestaurantsOrder.jsx
@@ -1,12 +1,12 @@
-// import React from 'react'
-// import OrdersTable from './OrderTable'
+import React from 'react'
+import OrdersTable from './OrderTable'
 
-// const RestaurantsOrder = () => {
-//   return (
-//     <div className='px-2'>
-//       <OrdersTable name={"All Orders"}/>
-//     </div>
-//   )
-// }
+const RestaurantsOrder = () => {
+  return (
+    <div className='px-2'>
+      <OrdersTable name={"All Orders"}/>
+    </div>
+  )
+}
 
-// export default RestaurantsOrder
+export default RestaurantsOrder
